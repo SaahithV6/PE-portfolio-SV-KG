fd0376b7b01e84aff4065f896a4fd6e8f05b810c
Saahith Veeramaneni <66752243+SaahithV6@users.noreply.github.com>
2026-06-19 12:12:17 -0700

    Enhance index route with experiences and education
    Added experiences and education data to the index route.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from dotenv import load_dotenv
+from .data import EXPERIENCES, EDUCATION, HOBBIES, LOCATIONS
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,4 +9,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template(
+        'index.html',
+        title="MLH Fellow",
+        url=os.getenv("URL"),
+        experiences=EXPERIENCES,
+        education=EDUCATION,
+    )
